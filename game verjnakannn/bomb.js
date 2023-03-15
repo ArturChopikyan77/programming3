49332217aa7bc7a80a3f969d8a84799757ee4a01
ArturChopikyan77 <artur.chopikyan@tumo.org>
2023-03-15 17:01:21 +0400

    imbertiance

--- game verjnakannn/bomb.js
+++ game verjnakannn/bomb.js
@@ -1,7 +1,6 @@
-class Bomb {
+class Bomb extends LivingCreature{
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        super(x,y)
         this.time = 5;
         this.radius = 1
         this.directions = [
--- game verjnakannn/security.js
+++ game verjnakannn/security.js
@@ -1,7 +1,6 @@
-class Security {
+class Security extends LivingCreature {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        super(x,y)
         this.multiply = 0
         this.directions = [];
     }
@@ -16,17 +15,8 @@ class Security {
 
     chooseCell(char) {
         this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+        
+        return super.chooseCell(char);
     }
 
     mul() {
